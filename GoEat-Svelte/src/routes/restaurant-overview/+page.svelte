2fa2d31647de5f2ebcf35a8462dfa73b30870179
JuicyBram <bvdwallen1912@gmail.com>
2024-05-30 12:52:02 +0200

    Add flexbox to restaurant-overview

--- GoEat-Svelte/src/routes/restaurant-overview/+page.svelte
+++ GoEat-Svelte/src/routes/restaurant-overview/+page.svelte
@@ -2,6 +2,19 @@
     import { Button } from '@sveltestrap/sveltestrap';
 </script>
 
-<Button color="success">
-    <span>See your Countries</span>
-</Button>
+<style>
+    .flex-container {
+        display: flex;
+        flex-direction: column;
+        height: calc(100vh - 60px);
+        background-color: aqua;
+    }
+    .flex-container > div {
+        background-color: blueviolet;
+    }
+</style>
+
+<div class="flex-container">
+    <div>1</div>
+    <div>2</div>
+</div>
