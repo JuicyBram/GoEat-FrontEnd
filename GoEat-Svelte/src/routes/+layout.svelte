33ca2c72b3c45b38a890b066636228ddbdd83c59
JuicyBram <bvdwallen1912@gmail.com>
2024-05-30 13:19:48 +0200

    Add flexboxes to restaurant-overview page

--- GoEat-Svelte/src/routes/+layout.svelte
+++ GoEat-Svelte/src/routes/+layout.svelte
@@ -51,3 +51,4 @@
         <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Instellingen</a>
     </div>
 </Navbar>
+<slot />
--- GoEat-Svelte/src/routes/restaurant-overview/+page.svelte
+++ GoEat-Svelte/src/routes/restaurant-overview/+page.svelte
@@ -3,18 +3,27 @@
 </script>
 
 <style>
+    body, html {
+        height: 100%;
+    }
     .flex-container {
         display: flex;
         flex-direction: column;
-        height: calc(100vh - 60px);
+        height: calc(100vh - 96px);
         background-color: aqua;
     }
-    .flex-container > div {
+    .restaurant-tags {
         background-color: blueviolet;
+        flex: 0 0 15%;
+    }
+    .restuarant-overview {
+        background-color: brown;
+        flex: 1;
+        display: flex;
     }
 </style>
 
 <div class="flex-container">
-    <div>1</div>
-    <div>2</div>
+    <div class="restaurant-tags">1</div>
+    <div class="restuarant-overview">2</div>
 </div>
